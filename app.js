8754b91cced874841ac1cc1741946a2e1e20713f
akshaysk-accubits <akshay.krishna@accubits.com>
2021-11-23 08:32:19 +0530

    Initialising

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//* Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log("Error ", err);
+  }
+  console.log("Node.js is running at PORT", process.env.PORT);
+});
